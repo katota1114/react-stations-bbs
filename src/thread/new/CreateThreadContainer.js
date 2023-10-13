9d3da17ddf025c9a3ff52c1732a7b64d2db7ab3d
katota1114 <katota1114@gmail.com>
2023-10-13 20:45:14 +0900

    station3 掲示投稿画面を作った

--- src/thread/new/CreateThreadContainer.js
+++ src/thread/new/CreateThreadContainer.js
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 function CreateThreadContainer() {
-    return <></>
+  const [threadTitle, setThreadTitle] = useState("");
+  const navigate = useNavigate();
+
+  function handleClickSubmitButton() {
+    if (threadTitle === "") {
+      return;
+    }
+    const body = { title: threadTitle };
+
+    fetch("https://railway.bulletinboard.techtrain.dev/threads", {
+      method: "POST",
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        navigate("/");
+      });
+  }
+
+  return (
+    <div className="container">
+      <h1>スレッド新規作成</h1>
+      <input
+        name="thread-title"
+        type="text"
+        className="thread-title-input"
+        required={true}
+        placeholder="スレッドタイトル"
+        value={threadTitle}
+        onChange={(event) => setThreadTitle(event.target.value)}
+      ></input>
+      <div className="action-buttons">
+        <Link to={"/"}>Topに戻る</Link>
+        <button onClick={handleClickSubmitButton}>作成</button>
+      </div>
+    </div>
+  );
 }
 
-export default CreateThreadContainer
+export default CreateThreadContainer;
